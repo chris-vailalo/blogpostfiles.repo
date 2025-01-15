1656734f2b29040565b105aca51d8eb45d6d6311
Chris <101206860+chris-vailalo@users.noreply.github.com>
2025-01-15 17:21:24 +1300

    remove sandbox repo

--- AWS Sandbox Accounts for Events/src/index.js
+++
@@ -1,7 +0,0 @@
-import "@cloudscape-design/global-styles/index.css"
-import { createRoot } from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
